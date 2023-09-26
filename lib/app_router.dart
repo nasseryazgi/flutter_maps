9299da2a669b1dd3e3561073be383e2d05eb6c1e
nasseryazgi <naserkoker.xp@gmail.com>
2023-09-27 02:19:57 +0300

    add constants

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -2,11 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_maps/presestation/screens/login_screen.dart';
 
+import 'constants/strings.dart';
+
 class AppRouter {
   Route? generateRouter(RouteSettings routeSettings) {
     switch (routeSettings.name) {
-      case '/':
-        return MaterialPageRoute(builder: (_) => LoginScreen());
+      case loginScreen:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
     }
   }
 }
